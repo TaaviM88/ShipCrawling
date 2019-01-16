895908a7aed996f00610acba095ff079f0f721e2
Ville W <ville.wessman@gmail.com>
2019-01-16 11:48:50 +0200

    HUD + HP Slider

---
+++ ShipCrawler/Assets/Scripts/Stats/UIStatUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStatUpdate : MonoBehaviour
+{
+    public GameObject Player;
+    private CharacterStats stats;
+
+    public Slider healthSlider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        stats = Player.GetComponent<CharacterStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = stats.currentHealth;
+
+        if (Input.GetKeyDown("return"))
+        {
+            stats.TakeDamage(20);
+        }
+    }
+}
