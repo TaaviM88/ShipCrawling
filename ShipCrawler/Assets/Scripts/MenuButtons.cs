597ad6dd72979b6731abffd78d27fb7cfb8d2e90
Ville W <ville.wessman@gmail.com>
2019-01-17 10:02:00 +0200

    Menu Scene

---
+++ ShipCrawler/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
