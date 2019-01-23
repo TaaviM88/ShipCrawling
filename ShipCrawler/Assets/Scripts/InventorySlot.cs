01c048fdcfc5c52d184fa3eb654e98f6dc1fe945
TaaviM88Laptop <juomakuski@hotmail.com>
2019-01-23 20:31:42 +0200

    Pick up script added InventorySlot script added

---
+++ ShipCrawler/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    Item item;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+        icon.sprite = item.icon;
+        icon.enabled = true;
+
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+
+    public void OnRemoveButton()
+    {
+        Inventory.instance.RemoveItem(item);
+    }
+
+    public void UseItem()
+    {
+        if(item != null)
+        {
+            item.Use();
+        }
+    }
+}
---
+++ ShipCrawler/Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ItemPickUp : Interactable
+{
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+    }
+
+    
+    private void PickUp()
+    {
+        Journal.Instance.Log($"Picking up { item.name}");
+
+        bool wasPickedUP = Inventory.instance.AddItem(item);
+
+        if (wasPickedUP)
+            Destroy(gameObject);
+    }
+}
