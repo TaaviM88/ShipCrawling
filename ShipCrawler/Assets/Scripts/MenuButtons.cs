5e08db0b63fb0b133d44c1563f03587d6f5d4298
Ville W <ville.wessman@gmail.com>
2019-01-29 09:51:26 +0200

    Credits panel added to menu.

--- ShipCrawler/Assets/Scripts/MenuButtons.cs
+++ ShipCrawler/Assets/Scripts/MenuButtons.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
+
+    public GameObject creditsPanel;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,12 @@ public class MenuButtons : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        creditsPanel.gameObject.SetActive(!creditsPanel.gameObject.activeSelf);
+
+ 
     }
 }
