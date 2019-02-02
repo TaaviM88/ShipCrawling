109b435e9d135b98de29885b2481313d50c7726d
TaaviM88Laptop <juomakuski@hotmail.com>
2019-02-02 02:56:57 +0200

    Everything is fucked Level 2 created Level 1 ending added Lightning changed

---
+++ ShipCrawler/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : Interactable
+{
+    public int NextSceneIndex = 0;
+    public static ChangeScene Instance { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        gameObject.SetActive(false);
+    }
+
+
+    public override void Interact()
+    {
+        base.Interact();
+        ChooseSceneToLoad(NextSceneIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void ChooseSceneToLoad (int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(NextSceneIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
--- ShipCrawler/Assets/Scripts/GameManager.cs
+++ ShipCrawler/Assets/Scripts/GameManager.cs
@@ -4,5 +4,53 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Add Stage boss and player and levelEndTrigger")]
+    public GameObject player;
+    public GameObject levelBoss;
+    public GameObject levelEndTrigger;
+    [Header("Next scene number")]
+    public int nextSceneIndex = 0;
 
+    bool isBossAlive, isPlayerAlive;
+
+    
+
+    public void Awake()
+    {
+        if(player == null)
+        {
+            Debug.Log($"{this.gameObject.name} can't find player.");
+        }
+        else
+        {
+            isPlayerAlive = false;
+        }
+
+        if (levelBoss == null)
+        {
+            Debug.Log($"{this.gameObject.name} can't find level boss.");
+        }
+        else
+        {
+            isBossAlive = true;
+        }
+    }
+
+
+    private void Update()
+    {
+        if(levelBoss == null && isBossAlive)
+        {
+            BossIsDead();
+            isBossAlive = false;
+        }
+    }
+
+
+    public void BossIsDead()
+    {
+        levelEndTrigger.SetActive(true);
+        Journal.Instance.Log("Go to boat and press action button. YOU FAGGOT!");
+        
+    }
 }
